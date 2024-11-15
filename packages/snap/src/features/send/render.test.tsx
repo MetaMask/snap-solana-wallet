4ffc0a0b7340bde0062e2ae898094e6109ec63eb
Alejandro Garcia Anglada <aganglada@gmail.com>
2024-11-15 14:30:26 +0100

    feat: account selector

---
+++ packages/snap/src/features/send/render.test.tsx
@@ -0,0 +1,71 @@
+import { installSnap } from '@metamask/snaps-jest';
+
+import {
+  SolanaCaip2Networks,
+  SolanaInternalRpcMethods,
+} from '../../core/constants/solana';
+import { TEST_ORIGIN } from '../../core/test/utils';
+import { SendForm } from './components/SendForm/SendForm';
+import { SendFormNames } from './types/form';
+
+const solanaKeyringAccounts = [
+  {
+    id: '123',
+    address: '123',
+    index: 0,
+    methods: [],
+    options: {},
+    type: 'solana:data-account' as const,
+  },
+];
+
+const mockContext = {
+  accounts: solanaKeyringAccounts,
+  scope: SolanaCaip2Networks.Devnet,
+  selectedAccountId: '123',
+  validation: {},
+};
+
+describe('Send', () => {
+  it('renders the send form', async () => {
+    const { request, mockJsonRpc } = await installSnap();
+
+    mockJsonRpc({
+      method: 'snap_manageState',
+      result: { keyringAccounts: solanaKeyringAccounts },
+    });
+
+    const response = request({
+      origin: TEST_ORIGIN,
+      method: SolanaInternalRpcMethods.StartSendTransactionFlow,
+      params: {
+        scope: SolanaCaip2Networks.Mainnet,
+        account: '123',
+      },
+    });
+
+    const screen = await response.getInterface();
+
+    expect(screen).toRender(<SendForm context={mockContext} />);
+
+    await screen.selectFromSelector(SendFormNames.AccountSelector, '123');
+
+    expect(screen).toRender(<SendForm context={mockContext} />);
+  });
+
+  // TODO: Fix this test
+  it.skip('fails when wrong params are given', async () => {
+    const { request } = await installSnap();
+
+    const response = request({
+      origin: TEST_ORIGIN,
+      method: SolanaInternalRpcMethods.StartSendTransactionFlow,
+      params: {
+        scope: 'wrong scope',
+        account: '123',
+      },
+    });
+
+    expect(response).toThrow();
+  });
+});
