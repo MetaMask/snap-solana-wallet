f147c8e3e670b1336a7a662bbbcf7b8eb8f3cdd2
Alejandro Garcia Anglada <aganglada@gmail.com>
2025-01-27 15:57:56 +0100

    chore: review changes

--- packages/snap/src/core/handlers/onAssetsConversion/onAssetsConversion.ts
+++ packages/snap/src/core/handlers/onAssetsConversion/onAssetsConversion.ts
@@ -1,12 +1,8 @@
 import type { OnAssetsConversionHandler } from '@metamask/snaps-sdk';
 
 import { tokenPricesService } from '../../../snapContext';
-import { OnAssetConversionStruct } from '../../validation/structs';
-import { validateRequest } from '../../validation/validators';
 
 export const onAssetsConversion: OnAssetsConversionHandler = async (params) => {
-  validateRequest(params, OnAssetConversionStruct);
-
   const { conversions } = params;
 
   const result = await tokenPricesService.getMultipleTokenConversions(
--- packages/snap/src/core/handlers/onAssetsLookup/onAssetsLookup.ts
+++ packages/snap/src/core/handlers/onAssetsLookup/onAssetsLookup.ts
@@ -1,13 +1,8 @@
-import type { CaipAssetType } from '@metamask/keyring-api';
 import type { OnAssetsLookupHandler } from '@metamask/snaps-sdk';
 
 import { tokenMetadataClient } from '../../../snapContext';
-import { OnAssetLookupStruct } from '../../validation/structs';
-import { validateRequest } from '../../validation/validators';
 
 export const onAssetsLookup: OnAssetsLookupHandler = async (params) => {
-  validateRequest(params, OnAssetLookupStruct);
-
   const { assets } = params;
 
   const metadata = await tokenMetadataClient.getTokenMetadataFromAddresses(
