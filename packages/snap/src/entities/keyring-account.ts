838f1387063fa6e79130a295247b50726f34f772
Xavier Brochard <xavier.brochard@consensys.net>
2025-06-06 10:29:58 +0200

    docs: clean up

--- packages/snap/src/entities/keyring-account.ts
+++ packages/snap/src/entities/keyring-account.ts
@@ -4,7 +4,6 @@ import type { EntropySourceId, KeyringAccount } from '@metamask/keyring-api';
  * We need to store the index of the KeyringAccount in the state because
  * we want to be able to restore any account with a previously used index.
  */
-
 export type SolanaKeyringAccount = {
   entropySource: EntropySourceId;
   derivationPath: `m/${string}`;
