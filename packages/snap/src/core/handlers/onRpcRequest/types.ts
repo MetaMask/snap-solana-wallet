e86758368946aade3976e7618873380724b0f3e3
Xavier Brochard <xavier.brochard@consensys.net>
2025-07-01 10:35:19 +0200

    docs: add jsdoc

--- packages/snap/src/core/handlers/onRpcRequest/types.ts
+++ packages/snap/src/core/handlers/onRpcRequest/types.ts
@@ -3,6 +3,10 @@ export enum RpcRequestMethod {
   GetFeeForTransaction = 'getFeeForTransaction',
 }
 
+/**
+ * Methods specific to the test dapp,
+ * to allow specific flows for manual testing.
+ */
 export enum TestDappRpcRequestMethod {
   TestSetupAllConnections = 'testSetupAllConnections',
   TestSubscribeToAccount = 'testSubscribeToAccount',
