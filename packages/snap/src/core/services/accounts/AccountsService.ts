d78eb4a24995dd6f995e0d924b2cd5aedd7ed549
Xavier Brochard <xavier.brochard@consensys.net>
2025-07-24 17:20:44 +0200

    feat: improve account sync

--- packages/snap/src/core/services/accounts/AccountsService.ts
+++ packages/snap/src/core/services/accounts/AccountsService.ts
@@ -1,35 +1,89 @@
+import { differenceBy, uniq } from 'lodash';
+
 import type { SolanaKeyringAccount } from '../../../entities';
-import type { IStateManager } from '../state/IStateManager';
-import type { UnencryptedStateValue } from '../state/State';
+import { createPrefixedLogger, type ILogger } from '../../utils/logger';
+import type { AssetsService } from '../assets/AssetsService';
+import type { TransactionsService } from '../transactions';
+import type { AccountsRepository } from './AccountsRepository';
+import { CouldNotFetchTransactionsError } from './errors/CouldNotFetchTransactionsError';
 
 export class AccountsService {
-  readonly #state: IStateManager<UnencryptedStateValue>;
+  readonly #accountsRepository: AccountsRepository;
 
-  constructor(state: IStateManager<UnencryptedStateValue>) {
-    this.#state = state;
+  readonly #transactionsService: TransactionsService;
+
+  readonly #assetsService: AssetsService;
+
+  readonly #logger: ILogger;
+
+  constructor(
+    accountsRepository: AccountsRepository,
+    transactionsService: TransactionsService,
+    assetsService: AssetsService,
+    logger: ILogger,
+  ) {
+    this.#accountsRepository = accountsRepository;
+    this.#transactionsService = transactionsService;
+    this.#assetsService = assetsService;
+    this.#logger = createPrefixedLogger(logger, '[🧑 AccountsService]');
   }
 
-  /**
-   * Returns all accounts from the state.
-   * @returns All accounts from the state.
-   */
   async getAll(): Promise<SolanaKeyringAccount[]> {
-    const accounts =
-      await this.#state.getKey<UnencryptedStateValue['keyringAccounts']>(
-        'keyringAccounts',
-      );
-
-    return Object.values(accounts ?? {});
+    return this.#accountsRepository.getAll();
   }
 
   async findById(id: string): Promise<SolanaKeyringAccount | null> {
-    const accounts = await this.getAll();
-    return accounts.find((account) => account.id === id) ?? null;
+    return this.#accountsRepository.findById(id);
   }
 
   async findByAddress(address: string): Promise<SolanaKeyringAccount | null> {
-    const accounts = await this.getAll();
+    return this.#accountsRepository.findByAddress(address);
+  }
 
-    return accounts.find((account) => account.address === address) ?? null;
+  async synchronize(accounts?: SolanaKeyringAccount[]): Promise<void> {
+    const accountsToSync = accounts ?? (await this.getAll());
+
+    this.#logger.log('Synchronizing accounts', accountsToSync);
+
+    try {
+      const transactionsBefore =
+        await this.#transactionsService.findByAccounts(accountsToSync);
+
+      this.#logger.log('transactionsBefore', transactionsBefore);
+
+      await this.#transactionsService
+        .synchronize(accountsToSync)
+        .catch((error) => {
+          throw new CouldNotFetchTransactionsError(error);
+        });
+
+      const transactionsAfter =
+        await this.#transactionsService.findByAccounts(accountsToSync);
+
+      this.#logger.log('transactionsAfter', transactionsAfter);
+
+      const diff = differenceBy(transactionsBefore, transactionsAfter, 'id');
+
+      this.#logger.log('diff', diff);
+
+      const accountIdsToRefresh = uniq(
+        diff.map((transaction) => transaction.account),
+      );
+
+      const accountsToRefresh = accountsToSync.filter((account) =>
+        accountIdsToRefresh.includes(account.id),
+      );
+
+      await this.#assetsService.refreshAssets(accountsToRefresh);
+    } catch (error) {
+      this.#logger.error('Error syncing accounts', error);
+
+      if (error instanceof CouldNotFetchTransactionsError) {
+        // Perform a full, refresh of the assets
+        await this.#assetsService.refreshAssets(accountsToSync);
+      } else {
+        throw error;
+      }
+    }
   }
 }
