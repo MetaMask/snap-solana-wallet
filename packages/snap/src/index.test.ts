121c768b0bc2e8d97eb012898095d63877e2f374
Xavier Brochard <xavier.brochard@consensys.net>
2025-08-29 12:21:41 +0200

    test: fix unit test

--- packages/snap/src/index.test.ts
+++ packages/snap/src/index.test.ts
@@ -73,70 +73,7 @@ describe('onCronjob', () => {
     ).rejects.toThrow(/Expected one of/u);
   });
 
-  it('calls the correct handler when the method is valid and snap is not locked', async () => {
-    const handler = jest.fn();
-    handlers[ScheduleBackgroundEventMethod.RefreshSend] = handler;
-
-    const snap = {
-      request: jest.fn().mockResolvedValue({ locked: false, active: true }),
-    };
-
-    (globalThis as any).snap = snap;
-
-    await onCronjob({
-      request: {
-        id: '1',
-        jsonrpc: '2.0',
-        method: ScheduleBackgroundEventMethod.RefreshSend,
-      },
-    });
-
-    expect(handler).toHaveBeenCalled();
-  });
-
-  it('does not call the handler when the client is locked', async () => {
-    const handler = jest.fn();
-    handlers[ScheduleBackgroundEventMethod.RefreshSend] = handler;
-
-    const snap = {
-      request: jest.fn().mockResolvedValue({ locked: true, active: true }),
-    };
-
-    (globalThis as any).snap = snap;
-
-    await onCronjob({
-      request: {
-        id: '1',
-        jsonrpc: '2.0',
-        method: ScheduleBackgroundEventMethod.RefreshSend,
-      },
-    });
-
-    expect(handler).not.toHaveBeenCalled();
-  });
-
-  it('does not call the handler when the client is inactive', async () => {
-    const handler = jest.fn();
-    handlers[ScheduleBackgroundEventMethod.RefreshSend] = handler;
-
-    const snap = {
-      request: jest.fn().mockResolvedValue({ active: false, locked: false }),
-    };
-
-    (globalThis as any).snap = snap;
-
-    await onCronjob({
-      request: {
-        id: '1',
-        jsonrpc: '2.0',
-        method: ScheduleBackgroundEventMethod.RefreshSend,
-      },
-    });
-
-    expect(handler).not.toHaveBeenCalled();
-  });
-
-  it('does call the handler when the client is unlocked and active', async () => {
+  it('calls the correct handler', async () => {
     const handler = jest.fn();
     handlers[ScheduleBackgroundEventMethod.RefreshSend] = handler;
 
