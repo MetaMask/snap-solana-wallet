da4af0b08e1865b2c77479daa6569f35dc5f7ccb
Charly Chevalier <charly.chevalier@consensys.net>
2026-03-27 17:52:27 +0100

    chore: cleanup

--- packages/snap/src/core/handlers/onKeyringRequest/structs.ts
+++ packages/snap/src/core/handlers/onKeyringRequest/structs.ts
@@ -2,12 +2,10 @@ import { KeyringRequestStruct } from '@metamask/keyring-api';
 import type { Infer } from '@metamask/superstruct';
 import {
   array,
-  literal,
   min,
   nonempty,
   number,
   object,
-  optional,
   string,
 } from '@metamask/superstruct';
 
